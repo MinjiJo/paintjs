28955c3d33faf33878bcdd50d9c398d7145ef4da
MinjiJo <MinjiJo@users.noreply.github.com>
2019-10-27 17:36:34 +0900

    Range a width and Filling all canvas

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("controls__color");
+const range = document.getElementById("jsRange");
+const btn = document.getElementById("jsMode");
 
+const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 800;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWedth = 2.5;
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
 
 var painting = false;
+var filling = false;
 
 //paint 시작할 때
 function startPainting() {
@@ -29,17 +34,41 @@ function onMouseMove(e) {
   if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
-    console.log("moveTo : " + x + y);
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
-    console.log("lineTo : " + x + y);
   }
 }
 
+//컬러 select
 function handleColor(e) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+//라인 width select
+function handleRangeChange(e) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+//fill과 paint를 교차하는 버튼 클릭시
+function handleBtnClick(e) {
+  if (filling == true) {
+    filling = false;
+    btn.innerHTML = "Fill";
+  } else {
+    filling = true;
+    btn.innerHTML = "Paint";
+  }
+}
+
+//fillRect()는 사각형으로 어느 정도의 크기를 나타낼지 정해줌
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 }
 
 Array.from(colors).forEach(color =>
@@ -51,4 +80,13 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (btn) {
+  btn.addEventListener("click", handleBtnClick);
 }
