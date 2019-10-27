cd2932f561eabdf79e38f603a1409f4263ca819d
MinjiJo <MinjiJo@users.noreply.github.com>
2019-10-27 17:19:01 +0900

    create drawing line

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("controls__color");
+
+const CANVAS_SIZE = 800;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWedth = 2.5;
 
 var painting = false;
+
+//paint 시작할 때
+function startPainting() {
+  painting = true;
+}
 
 //그림 그리는 것을 멈출 때
 function stopPainting() {
@@ -11,21 +26,29 @@ function stopPainting() {
 function onMouseMove(e) {
   const x = e.offsetX;
   const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    console.log("moveTo : " + x + y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    console.log("lineTo : " + x + y);
+  }
 }
 
-//마우스를 눌렀을 때 그림 그리는 기능 true
-function onMouseDown(e) {
-  painting = true;
+function handleColor(e) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
-//마우스에서 손을 땠을 때 painting = false
-function onMouseUp(e) {
-  stopPainting;
-}
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColor)
+);
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
